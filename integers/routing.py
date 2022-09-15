359bc981fd566eb69958a50e1f061348f49efba5
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-09-15 01:06:17 -0300

    fix: path url ws integers

--- integers/routing.py
+++ integers/routing.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .consumers import WSConsumer
 
-ws_urlpatterns = [path("ws/integers", WSConsumer.as_asgi())]
+ws_urlpatterns = [path("ws/integers/", WSConsumer.as_asgi())]
